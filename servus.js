ba3aaeaf11b035454a78d0e49a137de42d789c46
florind <anotherflo@yahoo.com>
2011-08-27 10:20:26 +0200

    Formatting

--- servus.js
+++ servus.js
@@ -1,12 +1,12 @@
 var sys = require('sys'),
-	http = require('http'),
-	rest = require('./restler/lib/restler'),
-	async = require('./async/lib/async'),
-	haml = require('./haml-js/lib/haml');
-	url = require('url'),
-	fs = require('fs');
+http = require('http'),
+rest = require('./restler/lib/restler'),
+async = require('./async/lib/async'),
+haml = require('./haml-js/lib/haml');
+url = require('url'),
+fs = require('fs');
 
-	// templating
+// templating
 var searchResHamlTemplate;
 fs.readFile('./search-res.haml', function(e, c) {
   searchResHamlTemplate = c.toString();
@@ -17,80 +17,80 @@ var sla = isNaN(sla) ? 2000 : sla; // 2s or the first argument in the command li
 sys.puts('Sla is: ' + sla);
 
 http.createServer(function (req, res) {
-	process.addListener('uncaughtException', function (err) {
-  	sys.puts('Caught exception: ' + err);
-		res.writeHead(500, 'text/plain');
-		res.end('error!');
-	});
+  process.addListener('uncaughtException', function (err) {
+    sys.puts('Caught exception: ' + err);
+    res.writeHead(500, 'text/plain');
+    res.end('error!');
+  });
 
-	var serviceUrls = {};
-	serviceUrls.goo	= 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=';
-	serviceUrls.twit = 'http://search.twitter.com/search.json?q=';
-	var urlParsed = url.parse(req.url, true);
-	var path = urlParsed.pathname;
+  var serviceUrls = {};
+  serviceUrls.goo	= 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=';
+  serviceUrls.twit = 'http://search.twitter.com/search.json?q=';
+  var urlParsed = url.parse(req.url, true);
+  var path = urlParsed.pathname;
 
-	var partialResults = [];
+  var partialResults = [];
   var timeoutReached = false;
 
-	switch(path) {
-		case '/':
-			res.writeHead(200, {'Content-Type': 'text/plain'});
-			res.end('Hello World\n');
-			break;
-		case '/aggregate':
-			//extract search predicate
-			var query = urlParsed.query;
-			if(query != undefined) {
-				predicate = query.q.replace(' ', '+');
-			} else {
-				res.writeHead(400); 
-				res.end('Query expected. Use q=... in the URL');
-				return;
-			}
-			async.parallel(buildParallelFuncArrayWithSla());
-			break;
-		default:
-			res.writeHead(404);
-			res.end('Not Found!');
-	}
+  switch(path) {
+    case '/':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Hello World\n');
+      break;
+    case '/aggregate':
+      //extract search predicate
+      var query = urlParsed.query;
+      if(query != undefined) {
+        predicate = query.q.replace(' ', '+');
+      } else {
+        res.writeHead(400); 
+        res.end('Query expected. Use q=... in the URL');
+        return;
+      }
+      async.parallel(buildParallelFuncArrayWithSla());
+      break;
+    default:
+      res.writeHead(404);
+    res.end('Not Found!');
+  }
 
-	function buildParallelFuncArrayWithSla() {
-	  var getResourceFunc = [];
-		for(var sn in serviceUrls) {
-			getResourceFunc.push(function(serviceName) {
-				return function(callback) {
+  function buildParallelFuncArrayWithSla() {
+    var getResourceFunc = [];
+    for(var sn in serviceUrls) {
+      getResourceFunc.push(function(serviceName) {
+        return function(callback) {
           callRestService(serviceUrls[serviceName] + predicate, serviceName, callback);
-      	};
-			}(sn));
-		}
+        };
+      }(sn));
+    }
     //will invoke callback after timeout
-		getResourceFunc.push(function(callback) {
-			setTimeout(function() {
-				if(!timeoutReached) {
-					//serve search results if any
-					serveContent(partialResults);
-					sys.puts("Timeout reached");
-				}
-			}, sla);
-		});
-		return getResourceFunc;
-	}
+    getResourceFunc.push(function(callback) {
+      setTimeout(function() {
+        if(!timeoutReached) {
+          //serve search results if any
+          serveContent(partialResults);
+          sys.puts("Timeout reached");
+        }
+      }, sla);
+    });
+    return getResourceFunc;
+  }
 
-	function serveContent(results) {
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		var searchItems = [];
-		for(i=0;i<results.length;i++) {
-			for(sr in results[i]) {
-				searchItems.push(results[i][sr]);
-			}
-		}
-		res.end(haml.render(searchResHamlTemplate, {locals: {items: searchItems}}));
-		timeoutReached = true;	//prevent further search results to be processed
-	}
+  function serveContent(results) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var searchItems = [];
+    for(i=0;i<results.length;i++) {
+      for(sr in results[i]) {
+        searchItems.push(results[i][sr]);
+      }
+    }
+    res.end(haml.render(searchResHamlTemplate, {locals: {items: searchItems}}));
+    timeoutReached = true;	//prevent further search results to be processed
+  }
 
-	function callRestService(url, serviceName, callback) {
+  function callRestService(url, serviceName, callback) {
     request = rest.get(url);
-		request.addListener('success', function(data) {
+    request.addListener('success', function(data) {
       searchResults = [];
       if (serviceName == 'goo') {
         dataJson = JSON.parse(data).responseData.results;
@@ -105,7 +105,7 @@ http.createServer(function (req, res) {
         for (sr in dataJson) {
           searchResult = {}
           searchResult.url = 'http://twitter.com/'
-              + dataJson[sr].from_user + '/status/' + dataJson[sr].id;
+            + dataJson[sr].from_user + '/status/' + dataJson[sr].id;
           searchResult.text = dataJson[sr].text;
           searchResults.push(searchResult);
         }
@@ -115,13 +115,13 @@ http.createServer(function (req, res) {
         sys.puts("Timeout not reached");
         serveContent(partialResults);
       }
-		});
-		
-		request.addListener('error', function(data) {
-			sys.puts('Error fetching [' + url + ']. Body:\n' + data);
-			callback(null, ' ');
-		});
-	}
+    });
+
+    request.addListener('error', function(data) {
+      sys.puts('Error fetching [' + url + ']. Body:\n' + data);
+      callback(null, ' ');
+    });
+  }
 
 }).listen(8124, "127.0.0.1");
 sys.puts('Server running at http://127.0.0.1:8124/');
